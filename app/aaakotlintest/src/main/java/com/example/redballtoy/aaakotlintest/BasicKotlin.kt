bc9f79c3898f6dab7e6f5a57b38e78b0ba94170d
Dmitry Sorokin <redballtoy@gmail.com>
2021-11-23 00:31:07 +0300

    Add AaaKotlinTest module

---
+++ app/aaakotlintest/src/main/java/com/example/redballtoy/aaakotlintest/BasicKotlin.kt
@@ -0,0 +1,11 @@
+package com.example.redballtoy.aaakotlintest
+
+/*
+    /*
+     Можно использовать вложенные комментарии
+    */
+*/
+
+fun main(args: Array<String>){
+    print("Привет Котлин!")
+}
