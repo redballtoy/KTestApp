f3dc22c06e729e5908be73f2af225a9311923720
Dmitry Sorokin <redballtoy@gmail.com>
2021-11-23 12:23:51 +0300

    Change project structure

--- app/activitylifecycle_sa_06k/src/main/java/com/example/activitylifecycle_sa_06k/MyActivityStateObserver.kt
+++ app/activitylifecycle_sa_06k/src/main/java/com/example/activitylifecycle_sa_06k/MyActivityStateObserver.kt
@@ -1,16 +1,12 @@
 package com.example.activitylifecycle_sa_06k
 
 import android.util.Log
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.OnLifecycleEvent
 
-class MyActivityStateObserver: LifecycleObserver{
-    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
-    fun getLifeCycleState(source: LifecycleOwner, event: Lifecycle.Event){
+class MyActivityStateObserver : DefaultLifecycleObserver {
+    fun getLifeCycleState(source: LifecycleOwner, event: Lifecycle.Event) {
         Log.d(MainActivity.MY_LOG_VIA_OBSERVER, event.toString())
-
     }
-
 }
